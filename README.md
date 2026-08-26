11de2b93692558f7cf53441f69a7f33d583f3b3a
Jeffrey Goldberg <jeffrey@goldmark.org>
2026-08-26 10:29:47 -0500

    mostly copied from rsa slides

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# modern-auth-talk
+# Material for talks on Building Modern Authentication
+
+Much of this focuses on the design and implementation processes that went
+into developing the. 1Password authentication system in 2015.
+
+Note that
+I no longer work for 1Password,
+I am not authorized to speak on their behalf,
+and I cannot say anything that has not previously been made public.
+The good news is that a great deal has been made public.
---
+++ src/Common/common.mk
@@ -0,0 +1,26 @@
+# COMMON_DIR should point back to this directory
+# from all sibling directories, but it can be
+# redefined in other makefiles if they are not in
+# sibling directories
+
+COMMON_DIR = ../Common
+LATEXMK_RC= $(COMMON_DIR)/latexmkrc
+
+
+PUB_STATIC_DIR= ../public/s
+
+$(PUB_STATIC_DIR)/%: %
+	cp $< $@
+
+%.pdf: %.tex
+	latexmk -r $(LATEXMK_RC) $(LATEXMK_OPTIONS) $<
+
+%.html: %.ipynb
+	jupyter nbconvert --to=html $<
+# For an explanation, see https://stackoverflow.com/a/73073900/1304076
+%.tex: %.ipynb
+	jupyter nbconvert --to=latex $<
+	perl -pi -e '/^([ \t]*)\\usepackage{unicode-math}/ and $$_.="$$1\\usepackage[default]{fontsetup}\n"' $@
+
+%.html %.pdf: %.Rmd
+	Rscript -e "rmarkdown::render('$<', output_file = '$@')"
